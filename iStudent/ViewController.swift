ba7256d2d162ffcc4e84a59701dcd3fdeeb966b9
Vincent Lee <vincent.h.lee@outlook.com>
2017-09-15 14:41:14 -0700

    In-progress sign in with Firebase

--- iStudent/ViewController.swift
+++ iStudent/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    var handle: AuthStateDidChangeListenerHandle?
+    
+    @IBOutlet weak var userField: UITextField!
+    @IBOutlet weak var passField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated : Bool ) {
+        super.viewWillAppear(animated)
+        handle = Auth.auth().addStateDidChangeListener { (auth, user) in
+            //self.userField.text = ;
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        Auth.auth().removeStateDidChangeListener(handle!);
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +38,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func signInBtn(_ sender: UIButton) {
+        let email = userField.text;
+        let password = passField.text;
+        /*Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            // ...
+        }*/
 
+    }
+    
 }
 
