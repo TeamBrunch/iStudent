83fc55cdaef6093cda886d64dd73e3a4197823ed
Vincent Lee <vincent.h.lee@outlook.com>
2017-09-15 13:22:12 -0700

    Initial Commit

---
+++ iStudent/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iStudent
+//
+//  Created by Vincent Lee on 2017-09-15.
+//  Copyright © 2017 TeamBrunch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
