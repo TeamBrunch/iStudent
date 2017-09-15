f2e17fce18553081cfbcbbe31ea9d5c6b4340d72
itsmattli <li.matthew.m@gmail.com>
2017-09-15 14:58:18 -0700

    added TraceLog

--- iStudent/AppDelegate.swift
+++ iStudent/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import TraceLog
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate
@@ -18,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate
     {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        TraceLog.configure(environment: ["LOG_ALL": "TRACE1"])
         return true
     }
 
